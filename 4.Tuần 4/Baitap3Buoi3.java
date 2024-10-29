e2858ba056b14c8a30d7412a8e9e5cf9cf401d47
VoHoTanLoc107896 <tanlochongphuc@gmail.com>
2024-10-29 21:47:07 +0700

    Add files via upload

---
+++ 4.Tuần 4/Baitap3Buoi3.java
@@ -0,0 +1,19 @@
+
+package Loc107896.buoi4;
+
+public class Baitap3Buoi3 { 
+     public static void main(String[] args) {
+  
+    int[][][] c = { {{5, 2, 3}, {4, 5, 6, 8}, {7, 7}},{{5, 6, 7, 3}, {8, 9}}};
+
+ 
+    System.out.println("Cac phan tu cua mang c la: ");
+    for (int i = 0; i < c.length; i++) {
+        for (int j = 0; j < c[i].length; j++) {
+            for (int k = 0; k < c[i][j].length; k++) {
+                System.out.println("c[" + i + "][" + j + "][" + k + "] = " + c[i][j][k]);
+            }
+        }
+    }
+}
+}
